ceb91f975e96b9a1a4b0a89c811ddefb2afa2063
raghavendra5240 <raghavendra5240@gmail.com>
2021-09-18 18:10:35 +0530

    day-1 addignment

--- 1a.js
+++ 1a.js
@@ -1,13 +1,17 @@
-const wordCount = (str, words) => {
-    let count = 0,
-      i = 0;
-    while (true) {
-      const r = str.indexOf(words, i);
-      if (r !== -1) [count, i] = [count + 1, r + 1];
-      else return count;
+const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
+function countWords(paragraph,w1,w2){
+    let array = (paragraph.replace(/[.]/g, '')).split(' ')
+    
+    
+    
+    let word1=array.filter((x)=>x==w1).length
+    console.log(word1)
+    let word2=array.filter((x)=>x==w2).length
+    if (word1>word2){
+      console.log('the word love more frequently occured than you')
+    }else{
+      console.log('the word you more frequently occured than love')
     }
-  }; 
-  console.log(wordCount('I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.','paragraph'));
-   console.log(wordCount('I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.', 'love'));
-
-  console.log(wordCount('I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.','you'));
+    
+    }
+    console.log(countWords(paragraph,'love', 'you'));
